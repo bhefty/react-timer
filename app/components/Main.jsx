7448f27a1acdbe1a2ca917a9a960e8fbcbc50ed1
Bill Hefty <billhefty@gmail.com>
2016-12-01 14:59:27 -0600

    add countdownform and tests

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,10 +5,9 @@ import Nav from 'Nav'
 let Main = (props) => {
   return (
     <div>
-      <div>
-        <div>
-          <Nav />
-          <p>Main.jsx Rendered</p>
+      <Nav />
+      <div className='row'>
+        <div className='column small-centered medium-6 large-4'>
           { props.children }
         </div>
       </div>
